40b1e884b88f17c225e564c9a8be45ff8ec899d4
DaJdanni <jkeeper2626@gmail.com>
2025-05-18 08:59:50 -0700

    Update AIBot.ts

--- src/api/AIBot.ts
+++ src/api/AIBot.ts
@@ -49,7 +49,7 @@ const fetchResponse = async (prompt: string) => {
     type = [natural diaster or danger? -> DANGER, shelter or basic location? -> NORMAL]
     `
 
-    const response = await axios.post("server/api/gemini/", {
+    const response = await axios.post("api/gemini/", {
       systemInstruction: cri,
       userPrompt: prompt
     });
